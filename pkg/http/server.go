de9fa19b2ddea97ff9c916e88ca4cefca554f3e8
Kiyor <cai@kiyor.com>
2022-07-06 05:01:53 +0000

    up

---
+++ pkg/http/server.go
@@ -0,0 +1,34 @@
+package http
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func logf(r *http.Request, format string, args ...interface{}) {
+	s, _ := r.Context().Value(http.ServerContextKey).(*http.Server)
+	if s != nil && s.ErrorLog != nil {
+		s.ErrorLog.Printf(format, args...)
+	} else {
+		log.Printf(format, args...)
+	}
+}
+
+func Error(w http.ResponseWriter, error string, code int) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(code)
+	fmt.Fprintln(w, error)
+}
+
+var htmlReplacer = strings.NewReplacer(
+	"&", "&amp;",
+	"<", "&lt;",
+	">", "&gt;",
+	// "&#34;" is shorter than "&quot;".
+	`"`, "&#34;",
+	// "&#39;" is shorter than "&apos;" and apos was not in HTML until HTML5.
+	"'", "&#39;",
+)
---
+++ pkg/http/sniff.go
@@ -0,0 +1,3 @@
+package http
+
+const sniffLen = 512
